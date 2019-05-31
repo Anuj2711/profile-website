cca6dedfb4927a0a58c03899a940771ec2b2d036
Anuj Kharbanda <anuj.kharbanda01@gmail.com>
2019-06-01 01:24:26 +0530

    Updates

---
+++ precache-manifest.bccaa10c251cc041856da7d29a6e5792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ddec60d8f4c04ddd736488cfe1b52a",
+    "url": "/profile-website/index.html"
+  },
+  {
+    "revision": "99c7e09c4786edcec271",
+    "url": "/profile-website/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "8eefe97564ecc8a72785",
+    "url": "/profile-website/static/css/main.e6aa00e7.chunk.css"
+  },
+  {
+    "revision": "99c7e09c4786edcec271",
+    "url": "/profile-website/static/js/2.4139f382.chunk.js"
+  },
+  {
+    "revision": "8eefe97564ecc8a72785",
+    "url": "/profile-website/static/js/main.9a8740d6.chunk.js"
+  },
+  {
+    "revision": "daaebcec8630a65a92af",
+    "url": "/profile-website/static/js/runtime~main.0b0ab09f.js"
+  }
+]);
